704e4c4b3c03feae071cc05363615f7a46767bb0
kordian swiatek <pan.holingo@gmail.com>
2026-01-18 21:28:35 +0100

    feat(Core): add new files Services for project

--- Hotel.Core/Services/IReservationService.cs
+++ Hotel.Core/Services/IReservationService.cs
@@ -1,6 +1,10 @@
-namespace Hotel.Core.Entities;
+using Hotel.Core.Entities;
+
+namespace Hotel.Core.Services;
 
 public interface IReservationService
 {
+    Task<Reservation> CreateReservationAsync(Reservation reservation);
     
+    Task<bool> IsRoomAvailableAsync(int roomId, DateOnly checkIn, DateOnly checkOut);
 }
--- Hotel.Core/Services/ReservationService.cs
+++ Hotel.Core/Services/ReservationService.cs
@@ -1,6 +1,22 @@
-namespace DefaultNamespace;
+using Hotel.Core.Entities;
+
+namespace Hotel.Core.Services;
 
 public class ReservationService
 {
-    
+    public bool DoesOverlap(DateOnly newStart, DateOnly newEnd, DateOnly existingStart, DateOnly existingEnd)
+    {
+        return newStart < existingEnd && existingStart < newEnd;
+    }
+
+    public bool IsCapacitySufficient(int guestsCount, int roomCapacity)
+    {
+        return guestsCount <= roomCapacity;
+    }
+
+    public bool IsStayDurationValid(DateOnly checkIn, DateOnly checkOut)
+    {
+        int duration = checkOut.DayNumber - checkIn.DayNumber;
+        return duration >= 1 && duration <= 30;
+    }
 }
