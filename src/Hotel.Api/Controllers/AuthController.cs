78bc6b9c957ceabb43e218399070ddcd41213a76
O(N²) <58780872+Holingo@users.noreply.github.com>
2026-01-26 21:41:54 +0100

    [Version 0.3] - Update the files for Authorization - fix the bug (#14)
    * refactor(API): update the file `AuthController.cs`
    
    * feat(API): Add new file for DTO LoginDto.cs
    
    * refactor(API): update file Program.cs for authorization
    
    * core(API): update settings for the project API

---
+++ src/Hotel.Api/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Hotel.Api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Hotel.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginDto login)
+    {
+        if (login.Username == "admin@admin" && login.Password == "password")
+        {
+            var token = GenerateToken(login.Username);
+            return Ok(new {token});
+        }
+        
+        return Unauthorized("Invalid username or password");
+    }
+
+    private string GenerateToken(string username)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, "Admin"),
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddHours(3),
+            signingCredentials: credentials);
+        
+        return new  JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
